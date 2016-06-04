ad76bfc8c17d18d9e7cb5db3e423859967523291
FRC4909 <billericabionics@gmail.com>
2016-06-04 14:36:06 -0400

    Chris and Scott made the robot 2 cims per side

--- src/org/usfirst/frc4909/PTCAgileBot/commands/Drive.java
+++ src/org/usfirst/frc4909/PTCAgileBot/commands/Drive.java
@@ -43,7 +43,7 @@ public class Drive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.drivetrain.tankDrive();
-//    	Robot.oi.vibrate((float).5);
+    	
     	}
 
     // Make this return true when this Command no longer needs to run execute()
